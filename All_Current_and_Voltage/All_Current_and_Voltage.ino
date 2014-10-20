9bdf5ea4cfe12d84dbd72d62b6d197712dc10c5e
Andrew5252 <drew.dechaine@gmail.com>
2014-10-20 11:44:48 -0400

    Added code for all three sensors and renamed what gets displayed as voltages and current.  Also tweaked the calibration factors.

--- All_Current_and_Voltage/All_Current_and_Voltage.ino
+++ All_Current_and_Voltage/All_Current_and_Voltage.ino
@@ -3,7 +3,7 @@
 
 Adafruit_INA219 ina219_A(0x40);
 Adafruit_INA219 ina219_B(0x41);
-Adafruit_INA219 ina219_C(0x42);
+Adafruit_INA219 ina219_C(0x44);
 
 void setup(void) 
 {
@@ -21,13 +21,13 @@ void setup(void)
 
 void loop(void) 
 {
-  int sensorValue1 = analogRead(A0);
+  int sensorValue3 = analogRead(A0);
   int sensorValue2 = analogRead(A1);
-  int sensorValue3 = analogRead(A2);
+  int sensorValue1 = analogRead(A2);
   
-  float voltage1 = sensorValue1 * (5.0 / 1023.0);
-  float voltage2 = sensorValue2 * (5.0 / 1023.0);
-  float voltage3 = sensorValue3 * (5.0 / 1023.0);
+  float voltage1 = sensorValue1 * (4.97 / 1023.0);
+  float voltage2 = sensorValue2 * (4.97 / 1023.0);
+  float voltage3 = sensorValue3 * (4.97 / 1023.0);
   
   float current1_mA = 0;
   float current2_mA = 0;
@@ -38,36 +38,20 @@ void loop(void)
   current3_mA = ina219_C.getCurrent_mA();
 
   
-  Serial.print("Voltage 1:    "); Serial.print(voltage1 * 2.02); Serial.println(" V");
+  Serial.print("Voltage 1:    "); Serial.print(voltage1 * 2.00); Serial.println(" V");
   Serial.print("Current 1:    "); Serial.print(current1_mA); Serial.println(" mA");
   Serial.println("");
   
-  Serial.print("Voltage 2:    "); Serial.print(voltage2 * 2.02); Serial.println(" V");
-  Serial.print("Current 1:    "); Serial.print(current2_mA); Serial.println(" mA");
+  Serial.print("Voltage 2:    "); Serial.print(voltage2 * 2.11); Serial.println(" V");
+  Serial.print("Current 2:    "); Serial.print(current2_mA); Serial.println(" mA");
   Serial.println("");
    
-  Serial.print("Voltage 3:    "); Serial.print(voltage3 * 2.02); Serial.println(" V");
-  Serial.print("Current 1:    "); Serial.print(current3_mA); Serial.println(" mA");
+  Serial.print("Voltage 3:    "); Serial.print(voltage3 * 2.00); Serial.println(" V");
+  Serial.print("Current 3:    "); Serial.print(current3_mA); Serial.println(" mA");
   Serial.println("");
-
+  Serial.println("");
+  Serial.println("");
   delay(2000);
 }
 
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
- */
 
-// the setup routine runs once when you press reset:
-
-// the loop routine runs over and over again forever:
-//void loop() {
-  // read the input on analog pin 0:
-  
-  // print out the value you read:
-  //Serial.println(sensorValue);
-  //delay(1);        // delay in between reads for stability
-//}
